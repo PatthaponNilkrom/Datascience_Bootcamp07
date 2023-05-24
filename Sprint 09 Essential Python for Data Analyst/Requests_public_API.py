db48771d8df79daa44977b4249a0e0378e6909fc
PatthaponNilkrom <87893131+PatthaponNilkrom@users.noreply.github.com>
2023-05-24 13:06:58 +0700

    Update Requests_public_API.py

--- Sprint 09 Essential Python for Data Analyst/Requests_public_API.py
+++ Sprint 09 Essential Python for Data Analyst/Requests_public_API.py
@@ -1,1 +1,27 @@
+# Python Project API: Requests public API
+import requests
+import json
+from time import sleep
+import pandas as pd
 
+nums = list(range(1,52))
+result = []
+for i in nums:
+    url = f"https://api.pokemontcg.io/v2/cards/xy1-{i}"
+    response = requests.get(url)
+    data = response.json()
+    row = [
+        data["data"]["name"],
+        data["data"]["hp"],
+        data["data"]["types"],
+        data["data"]["weaknesses"],
+        data["data"]["rarity"],
+        data["data"]["cardmarket"]["prices"]["averageSellPrice"]
+    ]
+    result.append(row)
+    #sleep(1)
+print(result)
+
+# built .csv file
+df = pd.DataFrame(result, columns = ["name", "hp", "types", "weaknesses","rarity","price($)"])
+df.to_csv("pokemontcg_data.csv")
